f2b239c23ac735f23dba8eb9c5f999b360d35dd9
Dragos <dragos199993@gmail.com>
2021-07-08 21:18:47 +0300

    Purge fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './storybook-static/index.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
